6d756e75d957c6b0a311801c4c7f6981c49303d4
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-11-18 12:18:29 +0000

    laid out steps for solving the problem

---
+++ script.js
@@ -0,0 +1,30 @@
+// Create a list of doors,
+const doors = [1, 2, 3];
+// Create a random index for the list,
+const randomIndex = Math.floor(Math.random() * array.length);
+const simulations = 100000;
+// Loop through simulations with the following operation:
+// Randomly choose a prize door
+
+// Randomly choose the staying door
+
+// Filter through the doors to get Monty's door which is:
+// a. not the prize door
+// b. not the staying door
+
+// Filter through the doors to get the switched door, which is:
+// a. not the staying door
+// b. not Monty's door
+
+// Separate variables for win and loss returning true or false on the following conditions:
+// a. switchWin = (switched choice = prize)
+// b. stayWin = (stay choice = prize)
+
+// Return the wins for both.
+
+// For each result, find the number of times the player won by staying or switching:
+
+// Get the average
+// Get the percentage
+
+// Log the results.
