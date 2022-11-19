8ea4ea9af19be0c64aa28d778cf7f1f906b2146b
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-11-19 14:55:00 +0000

    final version with results displaying on the page and polished logic

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
+const input = document.querySelector("input");
+const submit = document.querySelector("button");
+const pageResults = document.querySelector("#results");
+
+// <====== Event listeners ======>
+submit.addEventListener("click", (e) => {
+	e.preventDefault();
+	run(input.value);
+});
+
+// <====== Functions ======>
+
+// <--- Simulation logic --->
 function simulation(simulations) {
 	const randomIndex = (array) => Math.floor(Math.random() * array.length);
 	const doors = [1, 2, 3];
 	let results = [];
-	// Loop through simulations,
+
+	//<-- Loop through the simulations -->
 	for (let i = 0; i < simulations; i++) {
 		const prize = randomIndex(doors);
 		const choice = randomIndex(doors);
 
-		// Filter monty's, and switch choices
+		// <- Filter monty's, and switch choices ->
 		const monty = doors.filter((door) => door !== prize || choice)[0];
 		const switchChoice = doors.filter((door) => door !== monty || choice)[0];
 
-		// Filter wins with both methods
+		// <- Filter wins with both methods ->
 		const stayWin = choice === prize ? 1 : 0;
 		const switchWin = switchChoice === prize ? 1 : 0;
 
@@ -20,28 +34,21 @@ function simulation(simulations) {
 	return results;
 }
 
-
+// <--- Calculate the results --->
 function calculate(results) {
-	const decimal = (number) => number.toFixed(1);
-
-	// Calculate the wins, average and percentages
+	// <-- Wins -->
 	const switchWinTotal = results.filter((result) => result.switchWin === 1).length;
 	const stayWinTotal = results.filter((result) => result.stayWin === 1).length;
 
+	// <-- Average -->
 	const switchWinAverage = switchWinTotal / results.length;
 	const stayWinAverage = stayWinTotal / results.length;
 
+	// <-- Percentage -->
 	const switchWinPercentage = switchWinAverage * 100;
 	const stayWinPercentage = stayWinAverage * 100;
 
-	console.log(`Switch:
-  average:    ${decimal(switchWinAverage)}
-  percentage: ${decimal(switchWinPercentage)}%`);
-
-	console.log(`Stay:
-  average:    ${decimal(stayWinAverage)}
-  percentage: ${decimal(stayWinPercentage)}%`);
-
+	// <-- Return the results as an object -->
 	return (wins = {
 		switch: {
 			total: switchWinTotal,
@@ -56,18 +63,28 @@ function calculate(results) {
 	});
 }
 
-
+// <--- Run the simulation --->
 function run(simulations, depth = 0) {
-	console.group(`Simulations: ${simulations}`);
+	const decimal = (number) => number.toFixed(1);
 	const results = simulation(simulations);
 	const wins = calculate(results);
+
+	// <-- Log the results -->
+	console.group(`Simulations: ${simulations}`);
+
+	console.log(`Switch:
+  average:    ${decimal(wins.switch.average)}
+  percentage: ${decimal(wins.switch.percentage)}%`);
+
+	console.log(`Stay:
+  average:    ${decimal(wins.stay.average)}
+  percentage: ${decimal(wins.stay.percentage)}%`);
 
 	if (depth >= 1) {
 		console.group("In depth results");
 		console.table(wins);
 		console.groupEnd();
 	}
-
 	if (depth >= 2) {
 		console.group("In depther results");
 		console.table(results);
@@ -75,4 +92,21 @@ function run(simulations, depth = 0) {
 	}
 
 	console.groupEnd();
+
+	// <-- Display the results -->
+	pageResults.innerHTML = `
+  <p>Switch wins: ${wins.switch.total}</p>
+  <p>Stay wins: ${wins.stay.total}</p>
+  `;
 }
+
+// <====== On page load ======>
+
+console.log(`To run the simulation, use the command "run(number of simulations, OPTIONAL depth of results)"
+Example:
+run(1000, 2)
+run(1000)
+
+Depth 0: Only the total
+Depth 1: Total, average and percentage in a table
+Depth 2: Wins and losses in a table for each simulation`);
