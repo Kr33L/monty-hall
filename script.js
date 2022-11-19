fd859fbf707d4e66ed5901d893764aecf7872b04
Kr33L <105356599+Kr33L@users.noreply.github.com>
2022-11-19 13:58:56 +0000

    first version of the loop simulation

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
-// Create a random index for the list,
-const randomIndex = (array) => Math.floor(Math.random() * array.length);
-// Create a list of doors,
-const doors = [1, 2, 3];
-const simulations = 100000;
-// Loop through simulations with the following operation:
-// const results = simulations.map(() => {});
-// Randomly choose a prize door
-const prize = randomIndex(doors);
-// Randomly choose the staying door
-const choice = randomIndex(doors);
-// Filter through the doors to get Monty's door which is:
-//  a. not the prize door
-//  b. not the staying door
-const monty = doors.filter((door) => door !== prize && door !== choice)[0];
-// Filter through the doors to get the switched door, which is:
-//  a. not the staying door
-//  b. not Monty's door
-const switchChoice = doors.filter((door) => door !== choice && door !== monty)[0];
-// Separate variables for win and loss returning true or false on the following conditions:
-//  a. switchWin = (switched choice = prize)
-const switchWin = switchChoice === prize ? 1 : 0;
-//  b. stayWin = (stay choice = prize)
-const stayWin = choice === prize ? 1 : 0;
+function simulation(simulations) {
+	const randomIndex = (array) => Math.floor(Math.random() * array.length);
+	const doors = [1, 2, 3];
+	let results = [];
+	// Loop through simulations,
+	for (let i = 0; i < simulations; i++) {
+		const prize = randomIndex(doors);
+		const choice = randomIndex(doors);
 
-// Return the wins for both. return { switchWin, stayWin };
+		// Filter monty's, and switch choices
+		const monty = doors.filter((door) => door !== prize || choice)[0];
+		const switchChoice = doors.filter((door) => door !== monty || choice)[0];
 
-// For each result, find the number of times the player won by staying or switching:
-// Get the average
-// Get the percentage
+		// Filter wins with both methods
+		const stayWin = choice === prize ? 1 : 0;
+		const switchWin = switchChoice === prize ? 1 : 0;
 
-// Log the results.
+		results.push({ switchWin, stayWin });
+	}
+	return results;
+}
